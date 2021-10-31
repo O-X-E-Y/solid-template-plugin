433955ad48b79703f816d9fb68f2e5e492837e84
GitHub Action <action@github.com>
2021-10-31 19:46:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/oxey/solidtemplateplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/oxey/solidtemplateplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.oxey.solidtemplateplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oxey.solidtemplateplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/oxey/solidtemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/oxey/solidtemplateplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.oxey.solidtemplateplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oxey.solidtemplateplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
